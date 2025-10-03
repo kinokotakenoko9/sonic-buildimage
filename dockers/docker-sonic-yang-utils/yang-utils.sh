311ef1f76261e6180088d7ceefad4fc495fb5fe7
kinokotakenoko9 <chdmitry2@gmail.com>
2025-10-03 22:19:41 +0300

    add: gunicorn server, replace dev server

--- dockers/docker-sonic-yang-utils/yang-utils.sh
+++ dockers/docker-sonic-yang-utils/yang-utils.sh
@@ -1,34 +1,22 @@
 #!/usr/bin/env bash
 
-# Startup script for SONiC yang utils server
-EXIT_YANG_UTILS_VARS_FILE_NOT_FOUND=1
-YANG_UTILS_VARS_FILE=/usr/share/sonic/templates/yang_utils.j2
+DEFAULT_SERVER_PORT=5000
+SERVER_PORT=$DEFAULT_SERVER_PORT
 
-if [ ! -f "$YANG_UTILS_VARS_FILE" ]; then
-    echo "Yang utils vars template file not found"
-    exit $EXIT_YANG_UTILS_VARS_FILE_NOT_FOUND
+YANG_UTILS_VARS_FILE=/usr/share/sonic/templates/yang_utils_vars.j2
+if [ -f "$YANG_UTILS_VARS_FILE" ]; then
+    CONFIGURED_PORT=$(sonic-cfggen -d -t $YANG_UTILS_VARS_FILE | jq -r '.yang_utils.port // empty')
+    if [ -n "$CONFIGURED_PORT" ]; then
+        SERVER_PORT=$CONFIGURED_PORT
+    fi
 fi
 
-# Read basic server settings from yang utils vars entries
-YANG_UTILS_VARS=$(sonic-cfggen -d -t $YANG_UTILS_VARS_FILE)
-YANG_UTILS_VARS=${YANG_UTILS_VARS//[\']/\"}
+echo "Starting yang-utils Gunicorn server on port $SERVER_PORT"
 
-YANG_UTILS=$(echo $MGMT_VARS | jq -r '.yang_utils')
+APP_DIR="/usr/share/sonic/yang-utils/"
 
-if [ -n "$YANG_UTILS" ]; then
-    SERVER_PORT=$(echo $YANG_UTILS | jq -r '.port')
-    LOG_LEVEL=$(echo $YANG_UTILS | jq -r '.log_level')
-else
-    SERVER_PORT=5000
-    LOG_LEVEL=5
-fi
-
-YANG_UTILS_ARGS="-logtostderr"
-[ ! -z $SERVER_PORT ] && YANG_UTILS_ARGS+=" -port $SERVER_PORT"
-[ ! -z $LOG_LEVEL   ] && YANG_UTILS_ARGS+=" -v $LOG_LEVEL"
-
-echo "YANG_UTILS_ARGS = $YANG_UTILS_ARGS"
-
-export CVL_SCHEMA_PATH=/usr/sbin/schema
-
-exec /usr/sbin/yang_utils ${YANG_UTILS_ARGS}
+exec /usr/bin/gunicorn --workers 3 \
+                      --bind 0.0.0.0:${SERVER_PORT} \
+                      --chdir ${APP_DIR} \
+                      -m 007 \
+                      wsgi:app
