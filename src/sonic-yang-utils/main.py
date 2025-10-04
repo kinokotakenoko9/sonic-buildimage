58e951566f2503bc4fa941040f3eae5ff9622ca7
kinokotakenoko9 <chdmitry2@gmail.com>
2025-10-04 14:31:43 +0300

    ref: main module

--- src/sonic-yang-utils/main.py
+++ src/sonic-yang-utils/main.py
@@ -1,22 +1,12 @@
-from flask import Flask, jsonify
-import redis
+#!/usr/bin/env python3
 
-db_conn = redis.Redis(host='localhost', port=6379, db=0)
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "test"
-
-@app.route('/api/v1/interfaces')
-def get_interfaces():
-    try:
-        interface_keys = db_conn.keys('PORT_TABLE:*')
-        interfaces = [key.decode('utf-8').split(':')[-1] for key in interface_keys]
-        return jsonify({"interfaces": interfaces})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+@app.route("/")
+def hello_world():
+    return "<p>test</p>"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
