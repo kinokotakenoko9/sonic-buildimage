aa532cac9320aeefe3e022944c7d6b79a24fbed4
kinokotakenoko9 <chdmitry2@gmail.com>
2025-10-04 14:30:48 +0300

    ref: rm vars from start script

--- dockers/docker-sonic-yang-utils/yang-utils.sh
+++ dockers/docker-sonic-yang-utils/yang-utils.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env bash
 
+APP_DIR="/usr/share/sonic/yang-utils"
+APP_MODULE="main:app"
+
 DEFAULT_SERVER_PORT=5000
 SERVER_PORT=$DEFAULT_SERVER_PORT
 
-YANG_UTILS_VARS_FILE=/usr/share/sonic/templates/yang_utils_vars.j2
-if [ -f "$YANG_UTILS_VARS_FILE" ]; then
-    CONFIGURED_PORT=$(sonic-cfggen -d -t $YANG_UTILS_VARS_FILE | jq -r '.yang_utils.port // empty')
-    if [ -n "$CONFIGURED_PORT" ]; then
-        SERVER_PORT=$CONFIGURED_PORT
-    fi
-fi
+echo "Starting Yang-Utils Gunicorn server on port $SERVER_PORT"
 
-echo "Starting yang-utils Gunicorn server on port $SERVER_PORT"
-
-APP_DIR="/usr/share/sonic/yang-utils/"
-
-exec /usr/bin/gunicorn --workers 3 \
+exec /usr/bin/gunicorn --workers 2 \
                       --bind 0.0.0.0:${SERVER_PORT} \
                       --chdir ${APP_DIR} \
-                      -m 007 \
-                      wsgi:app
+                      ${APP_MODULE}
