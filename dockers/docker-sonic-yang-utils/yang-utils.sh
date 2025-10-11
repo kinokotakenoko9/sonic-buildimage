d52b637c0088519516fad7167edfb1807757e256
kinokotakenoko9 <chdmitry2@gmail.com>
2025-10-11 13:30:39 +0300

    fix: service startup script

--- dockers/docker-sonic-yang-utils/yang-utils.sh
+++ dockers/docker-sonic-yang-utils/yang-utils.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-APP_DIR="/usr/share/sonic/yang-utils"
-APP_MODULE="main:app"
-
-DEFAULT_SERVER_PORT=5000
-SERVER_PORT=$DEFAULT_SERVER_PORT
+SERVER_PORT=5000
 
 echo "Starting Yang-Utils Gunicorn server on port $SERVER_PORT"
 
-exec /usr/bin/gunicorn --workers 2 \
-                      --bind 0.0.0.0:${SERVER_PORT} \
-                      --chdir ${APP_DIR} \
-                      ${APP_MODULE}
+exec python /usr/sbin/main.py
